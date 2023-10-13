6aa995ade4667313b53e10a9fd0a0da02bf6663e
Sanjid Dewan <dsanjid@gmail.com>
2023-10-13 12:14:19 -0400

    added project links

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function openInNewBackgroundTab(link) {
     link.dispatchEvent(evt);
     return false;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const projectLinks = document.querySelectorAll("[data-screenshot]");
+    const projectScreenshot = document.getElementById("project-screenshot");
+    const screenshotDisplay = document.querySelector(".screenshot-display");
+
+    projectLinks.forEach((link) => {
+        link.addEventListener("mouseenter", function () {
+            const screenshotSrc = this.getAttribute("data-screenshot");
+            projectScreenshot.src = screenshotSrc;
+            screenshotDisplay.style.display = "block"; // Show the screenshot display
+        });
+
+        link.addEventListener("mouseleave", function () {
+            screenshotDisplay.style.display = "none"; // Hide the screenshot display
+        });
+    });
+});
